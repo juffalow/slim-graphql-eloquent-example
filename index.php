15353ed942282e61cf49f4dc7af08469848c3b67
sheikalthaf <sheikalthaf@outlook.com>
2019-01-12 23:25:09 +0530

    enable cors

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ $container = require('./config/container.php');
 
 $app = new \Slim\App($container);
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->post('/graphql', 'GraphQLController:process');
 
 $app->run();
